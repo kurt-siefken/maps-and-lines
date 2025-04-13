7624206e679a6f1f540e369c6a2ac8b83e2fc480
kurt-siefken <109696124+kurt-siefken@users.noreply.github.com>
2025-04-13 17:21:58 -0400

    Update question-list.js

--- questions/question-list.js
+++ questions/question-list.js
@@ -12,16 +12,17 @@ AskQuestion[5] = "What is your favorite article of clothing you own?";
 AskQuestion[6] = "What is your breakfast food of choice?";
 AskQuestion[6] = "What wild animal would you be terrified to face?";
 AskQuestion[7] = "What subject can you ace on trivia night?";
-AskQuestion[8] = "Where do you want to visit before you die?";
+AskQuestion[8] = "What is a place you've always wanted to visit?";
 AskQuestion[9] = "If you had to eat a crayon, what color would you choose?";
 AskQuestion[10] = "If you had to move to a new state, where would you move?";
 AskQuestion[11] = "What is the best smell?";
-AskQuestion[12] = "What is the last thing you read?";
+AskQuestion[12] = "Fill in the blank: When I dance, I look ___.";
 AskQuestion[13] = "What is the best thing to play on at a playground?";
 AskQuestion[14] = "What is your guilty pleasure food?";
 AskQuestion[15] = "What was the last movie you watched and loved?";
 AskQuestion[16] = "What store can you spend a lot of time looking around?";
 AskQuestion[17] = "What is your favorite non-alcoholic drink?";
+AskQuestion[18] = "What is a charity or cause you feel strongly about?";
 
 
 /*---
